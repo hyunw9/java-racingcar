718c23b04c84cc30be5104fe6a6656c0238438c3
hyunw9 <rkdgusdnr32@naver.com>
2023-09-21 16:49:39 +0900

    refactor: 불필요한 개행 변경
    보내주신 블로그의 4.1.2 단락 참고하여
    불필요한 개행 문자 수정하였습니다.

---
+++ src/main/java/RacingMain.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class RacingMain {
+
+    public static void main(String []args) throws IOException {
+        GameController gameController = new GameController();
+        gameController.startGame();
+    }
+}
