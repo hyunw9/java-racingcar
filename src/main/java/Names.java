7b295c7b79dcde10456b9fd2680538da814d49d9
hyunw9 <rkdgusdnr32@naver.com>
2023-09-22 18:20:49 +0900

    refactor : Names 일급 컬렉션 생성
    Names 일급 컬렉션을 생성해 중복 검사를 진행하도록 변경했습니다!

---
+++ src/main/java/Names.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Names {
+
+    private final List<Name> names;
+
+    public Names(String nameLine) {
+        this.names = new ArrayList<>(Arrays.stream(nameLine.split(","))
+                .map(String::trim)
+                .map(Name::new)
+                .collect(Collectors.toList()));
+        validateNameDuplication(names);
+    }
+
+
+    public void validateNameDuplication(List<Name> names) {
+        Set<Name> nameSet = new HashSet<>(names);
+        if (nameSet.size() > names.size()) {
+            throw new IllegalArgumentException("이름이 중복됩니다.");
+        }
+    }
+
+    public List<Name> getNames() {
+        return names;
+    }
+}
