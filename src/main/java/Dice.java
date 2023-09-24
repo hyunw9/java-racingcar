7041395fae84c12c7abe7a77613f83a33f2ba5da
hyunw9 <rkdgusdnr32@naver.com>
2023-09-25 00:49:51 +0900

    refactor: 정적 팩토리 메서드 수정

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -5,8 +5,15 @@ public class Dice {
     private static final int DELIMITER = 4;
     private static final int BOUND = 10;
 
-    public Dice() {
-        this.dice = random.nextInt(10);
+    private final int number;
+
+    private Dice(int random) {
+        this.number = random;
+    }
+
+    public static Dice createRandomDice(){
+        int random = new Random().nextInt(BOUND);
+        return new Dice(random);
     }
 
     public boolean validateMove() {
