8045e59f70eadfaad6f9379be365167d23c72d3c
hyunw9 <rkdgusdnr32@naver.com>
2023-09-25 00:53:42 +0900

    refactor: 컨벤션 및 클린코드 적용
    객생체 4번 내용 명심하겠습니다!

--- src/main/java/GameView.java
+++ src/main/java/GameView.java
@@ -1,9 +1,7 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GameView {
+
     public void askCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요 (이름은 쉼표(,)를 기준으로 구분).");
     }
@@ -21,7 +19,8 @@ public class GameView {
     }
 
     public void showWinner(List<String> winner) {
-        String s = " ";
-        System.out.println(winner.stream().collect(Collectors.joining(",")) + "가 최종 우승했습니다.");
+        String message = String.join(",", winner);
+        message += "가 최종 우승했습니다.";
+        System.out.println(message);
     }
 }
