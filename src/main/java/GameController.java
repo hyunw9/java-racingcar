5928b75ad33edd768c2b582d44e0a9fc0f6eebb8
hyunw9 <rkdgusdnr32@naver.com>
2023-09-25 00:53:02 +0900

    refactor: 일급 컬렉션 및 의존 부분 수정
    리뷰해주신 내용 토대로 수정하였습니다!

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -13,32 +13,33 @@ public class GameController {
 
         gameView.askCarNames();
 
-        Names nameList = Names.createNames(inputView.getNames());
-        Cars cars = Cars.createCars(nameList);
+        String nameInput = inputView.getNames();
+        cars = Cars.createCars(nameInput);
 
         gameView.askTryCounts();
         int times = inputView.getTryTimes();
 
         gameView.showRacingStart();
         for (int i = 0; i < times; i++) {
-            setProgress(cars);
-            printCurrentProgress(cars);
+            setProgress();
+            printCurrentProgress();
             System.out.println();
         }
-        List<String> winners = new ArrayList<>(cars.getWinners(cars));
+        List<String> winners = new ArrayList<>(cars.getWinners());
         gameView.showWinner(winners);
     }
 
-    public void setProgress(Cars cars) {
+    public void setProgress() {
         cars.getCars()
                 .stream()
-                .filter(car -> dice.validateMove())
+                .filter(car -> Dice.createRandomDice()
+                        .validateMove())
                 .forEach(Car::incrementCount);
     }
 
-    private void printCurrentProgress(Cars cars){
+    private void printCurrentProgress(){
         for (Car car : cars.getCars()) {
-            String progress = car.getProgress();
+            String progress = car.getName() + ": " + "-".repeat(car.getCount());
             gameView.showProgress(progress);
         }
     }
